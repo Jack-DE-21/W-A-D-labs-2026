f3bd430d4218b3f71e40312df05c5f3f9211a607
Your Name <youremail@example.com>
2026-03-12 19:16:23 +0000

    Updated college work - add and delete functionality for playlists and songs

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,20 +2,38 @@
 
 import logger from "../utils/logger.js";
 import playlistStore from "../models/playlist-store.js";
+import { v4 as uuidv4 } from 'uuid';
 
 const dashboard = {
+
   createView(request, response) {
-    logger.info("Dashboard page loading!");
-
     const viewData = {
-      title: "Playlist App Dashboard",
-      playlists: playlistStore.getAllPlaylists()
+      title: 'Playlist Dashboard',
+      playlists: playlistStore.getAllPlaylists(),
     };
-
-    logger.debug(viewData.playlists);
-
     response.render('dashboard', viewData);
   },
+
+  addPlaylist(request, response) {
+    const newPlayList = {
+      id: uuidv4(),
+      title: request.body.title,
+      songs: [],
+    };
+    playlistStore.addPlaylist(newPlayList);
+    response.redirect('/dashboard');
+  },
+
+
+deletePlaylist(request, response) {
+  const playlistId = request.params.id;
+  logger.debug(`Deleting Playlist ${playlistId}`);
+  playlistStore.removePlaylist(playlistId);
+  response.redirect('/dashboard');
+},
+
+
+
 };
 
 export default dashboard;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 'use strict';
 
-
-
 import express from 'express';
 import routes from "./routes.js";
 import logger from "./utils/logger.js";
 import { create } from 'express-handlebars';
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const handlebars = create({extname: '.hbs'});
+const handlebars = create({ extname: '.hbs' });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 app.set("views", "./views");
